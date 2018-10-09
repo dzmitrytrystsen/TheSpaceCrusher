a071e73339b26c79220ad4758378b81a23bfb813
dzmitrytrystsen <tristendmitry@gmail.com>
2018-10-09 16:45:02 +0300

    Move() ready

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+
+
+    private float xMin;
+    private float xMax;
+    private float yMin;
+    private float yMax;
+
+    [SerializeField] private float paddingX = 1f;
+    [SerializeField] private float paddingY = 1f;
+
+    void Start ()
+	{
+	    SetUpMoveBorders();
+	}
+
+    private void SetUpMoveBorders()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + paddingX;
+        xMax = gameCamera.ViewportToScreenPoint(new Vector3(0.0075f, 0, 0)).x - paddingX;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + paddingY;
+        yMax = gameCamera.ViewportToScreenPoint(new Vector3(0, 0.0055f, 0)).y + paddingY;
+    }
+
+    void Update ()
+	{
+	    Move();
+	}
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax); 
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
